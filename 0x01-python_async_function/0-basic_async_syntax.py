4c9fee6374efb2298522b4db6bcacb7ce7847591
NavanMapp <navanmapp@gmail.com>
2023-10-10 10:58:38 +0000

    0x01-python_async_function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
-"""
-Basic Asynchronous Syntax
-This module provides a basic asynchronous
-coroutine for generating random delays.
-"""
+
 import asyncio
 import random
-async def wait_random(max_delay=10):
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Asynchronous coroutine that waits for a random delay between 0 and max_delay seconds.
+
+    Args:
+        max_delay (int): Maximum delay in seconds (inclusive). Default is 10.
+
+    Returns:
+        float: The random delay in seconds.
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
+
+if __name__ == "__main__":
+    print(asyncio.run(wait_random()))
+    print(asyncio.run(wait_random(5)))
+    print(asyncio.run(wait_random(15)))
