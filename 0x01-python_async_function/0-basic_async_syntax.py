2de6974eb84a13844cf409beb2f23932e5da77e4
NavanMapp <navanmapp@gmail.com>
2023-10-10 13:20:22 +0000

    0x01-python_async_function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,7 +10,6 @@ async def wait_random(max_delay: int = 10) -> float:
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
-
 if __name__ == "__main__":
     print(asyncio.run(wait_random()))
     print(asyncio.run(wait_random(5)))
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -3,8 +3,39 @@
 import asyncio
 import time
 from typing import List
-import wait_n from 1-concurrent_coroutines
-from 0-basic_async_syntax import wait_random
+import random
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Asynchronous coroutine that waits for a random
+    delay between 0 and max_delay seconds
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawn the wait_random coroutine 'n' times with specified 'max_delay'.
+    Return a list of delays in ascending order.
+
+    Args:
+        n (int): The number of times to spawn the coroutine.
+        max_delay (int): The maximum delay time for each coroutine.
+
+    Returns:
+        List[float]: A list of delays in ascending order.
+    """
+    delays = []
+
+    tasks = [wait_random(max_delay) for _ in range(n)]
+
+    completed_tasks = await asyncio.gather(*tasks)
+
+    for task in completed_tasks:
+        delays.append(task)
+
+    delays.sort()
+
+    return delays
 def measure_time(n: int, max_delay: int) -> float:
     """
     Measure the total execution time for wait_n(n, max_delay)
