be8496543661217366915eab8a3b3ca9b2455cc4
NavanMapp <navanmapp@gmail.com>
2023-10-10 12:35:40 +0000

    0x01-python_async_function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,7 +2,14 @@
 
 import asyncio
 from typing import List
-from 0-basic_async_syntax import wait_random
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Asynchronous coroutine that waits for a random
+    delay between 0 and max_delay seconds
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawn the wait_random coroutine 'n' times with specified 'max_delay'.
