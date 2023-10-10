b0b65c425014d6aeee58d7e26ecf18db7ebba875
NavanMapp <navanmapp@gmail.com>
2023-10-10 13:38:41 +0000

    0x01-python_async_function

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,27 +2,32 @@
 
 import asyncio
 from typing import List
-
-from 3-tasks import task_wait_random
-
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
+import random
+async def wait_random(max_delay: int = 10) -> float:
     """
-    Create an asyncio.Task for wait_random n times with the specified
-    max_delay and return the sorted list of delays.
+    Asynchronous coroutine that waits for a random
+    delay between 0 and max_delay seconds
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Create an asyncio.Task from the wait_random coroutine with the specified max_delay.
 
     Args:
-        n (int): Number of times to call task_wait_random.
-        max_delay (int): Maximum delay in seconds
-        for each task_wait_random call.
+        max_delay (int): Maximum delay in seconds for wait_random.
 
     Returns:
-        List[float]: List of delays in ascending order.
+        asyncio.Task: A task representing the execution of wait_random.
     """
+    return asyncio.ensure_future(wait_random(max_delay))
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    results = []
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    await asyncio.gather(*tasks)
-    delays = [task.result() for task in tasks]
-    return sorted(delays)
-
+    results = await asyncio.gather(*tasks)
+    results.sort()
+    return results
 if __name__ == "__main__":
     n = 5
     max_delay = 6
