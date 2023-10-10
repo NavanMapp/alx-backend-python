a8d56fcf55505f596cc6589ad1437986373415ee
NavanMapp <navanmapp@gmail.com>
2023-10-10 13:47:02 +0000

    0x02-python_async_comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,7 +4,6 @@ import asyncio
 from typing import List
 from random import uniform
 from async_generator import async_generator  # Import the async_generator coroutine
-
 async def async_comprehension() -> List[float]:
     """
     This coroutine collects 10 random numbers using an async comprehension
@@ -12,9 +11,7 @@ async def async_comprehension() -> List[float]:
     """
     random_numbers = [number async for number in async_generator()]
     return random_numbers
-
 async def main():
     print(await async_comprehension())
-
 if __name__ == "__main__":
     asyncio.run(main())
