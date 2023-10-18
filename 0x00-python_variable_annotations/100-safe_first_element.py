670f96f3ff12aa50ac2aaf52cc38bb44c109ee53
NavanMapp <navanmapp@gmail.com>
2023-10-18 08:30:06 +0000

    0x00-python_variable update

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-from typing import Any, Union
+"""Defines duck typed function"""
+from typing import Any, Sequence, Union
 
-def safe_first_element(lst: list) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """The types of the elements of the input are not known"""
     if lst:
         return lst[0]
     else:
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-from typing import Iterable, Sequence, List, Tuple
+"""Type-annotated function element_length"""
+from typing import Iterable, List, Sequence, Tuple
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples of sequence and int"""
     return [(i, len(i)) for i in lst]
